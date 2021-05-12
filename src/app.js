9b36a49a9fdde37aad38ae1e2625e9c6ffcfdf30
seateacup <83850140+seateacup@users.noreply.github.com>
2021-05-12 12:36:02 -0700

    Added search engine

--- src/app.js
+++ src/app.js
@@ -32,12 +32,20 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
+    let apiKey = "7cfa0d2aaa29695fdee2ebd04a28582f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(displayTemperature);
+}
 
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+    console.log(cityInputElement.value);
+}
 
+search("New York");
 
-let apiKey = "7cfa0d2aaa29695fdee2ebd04a28582f";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-
-
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
